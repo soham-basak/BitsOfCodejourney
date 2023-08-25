b8d04b3db8bcbf3233eeb385acf4f1ba92fae623
Dexter <71167911+soham-basak@users.noreply.github.com>
2023-08-25 23:57:20 +0530

    feat: :art: blog page ui

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,70 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 const BlogPost = () => {
   return (
-    <div>
-      <h1>Blog Post</h1>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+            voluptatum, totam nisi doloribus voluptate ullam hic quod
+            necessitatibus fugiat cumque. Ipsam libero officia dicta natus,
+            voluptatum voluptatibus sequi quia ex?
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/17873040/pexels-photo-17873040/free-photo-of-decepcion.jpeg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Dexter</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+          eveniet debitis, velit quia ex illum. Sint, repellat, quaerat culpa
+          voluptas id voluptatem qui, minus vitae tempora labore similique ea
+          provident? Quam porro exercitationem obcaecati maxime, ad aliquid quia
+          neque tenetur fugiat reprehenderit ex quae molestiae cum.
+          <br />
+          <br />
+          Accusamus odit commodi incidunt ullam? Optio, tempore non possimus
+          aperiam accusantium vel officiis quod, expedita at nihil illum
+          voluptatum porro, natus quia autem recusandae? Dignissimos fugiat
+          earum doloremque illum, sed, maiores id natus accusantium ducimus
+          dolorum nisi consequuntur, maxime magni obcaecati perspiciatis nulla
+          assumenda. Soluta est dolorum quo illum obcaecati, architecto facere
+          pariatur, labore quia aliquam aut officia modi voluptatibus placeat
+          temporibus. Neque hic ipsum sed rem, harum asperiores quidem dolor
+          autem architecto saepe velit ipsam repellat. Laborum iste corrupti
+          dolorem eos, hic at, odit obcaecati placeat esse praesentium repellat
+          similique natus repellendus quo quas alias cum. Quisquam harum quis,
+          voluptatum odit quo sed deserunt atque fugit corporis exercitationem
+          labore animi iusto dolorem enim id assumenda obcaecati.
+          <br />
+          <br />
+          Exercitationem officiis tempora eveniet unde qui numquam sit corrupti
+          in quo nemo eligendi et, a cupiditate, nobis, minima expedita quasi
+          officia animi possimus! Praesentium enim voluptatum cumque magni
+          exercitationem repudiandae sed iste, neque eligendi eum numquam modi.
+        </p>
+      </div>
     </div>
   );
 };
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,8 +4,38 @@ import Image from "next/image";
 
 const Blog = () => {
   return (
-    <div className={styles.container}>
-      <Link href="testId" className={styles.container}>
+    <div className={styles.mainContainer}>
+      <Link href="blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>TestBlog</h1>
+          <p className={styles.desc}>TestDesc</p>
+        </div>
+      </Link>
+      <Link href="blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>TestBlog</h1>
+          <p className={styles.desc}>TestDesc</p>
+        </div>
+      </Link>
+      <Link href="blog/testId" className={styles.container}>
         <div className={styles.imageContainer}>
           <Image
             src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
